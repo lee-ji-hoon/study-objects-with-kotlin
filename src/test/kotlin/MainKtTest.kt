8bdfd9279407cea6716992b3dc5f516cd3ae1910
ezhoon <dlwlgns1240@gmail.com>
2023-03-12 13:21:34 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class MainKtTest {
+
+    @Test
+    @DisplayName("이거 잘 됨?")
+    fun `이거 잘 됨?`() {
+        assertTrue(true)
+    }
+}
