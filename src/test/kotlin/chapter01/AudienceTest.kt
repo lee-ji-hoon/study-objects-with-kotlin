dbb97c598c61ac675bbf3679eb02b9d77b2593e9
ezhoon <dlwlgns1240@gmail.com>
2023-03-12 14:40:09 +0900

    [1장_이지훈] 소극장 1차 코드 테스트 작성 - 초대권이 없는 경우 -> 금액 줄어들고, hasTicket true - 초대권이 있는 경우 -> 금액 보존되고, hasTicket true

---
+++ src/test/kotlin/chapter01/AudienceTest.kt
@@ -0,0 +1,58 @@
+package chapter01
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+@DisplayName("관람객 클래스 테스트")
+class AudienceTest {
+
+    private lateinit var ticketSeller: TicketSeller
+    private lateinit var ticketOffice: TicketOffice
+    private lateinit var theater: Theater
+
+    @BeforeEach
+    fun initTheater() {
+        ticketOffice = TicketOffice(10, tickets)
+        ticketSeller = TicketSeller(ticketOffice)
+        theater = Theater(ticketSeller)
+    }
+
+    @Test
+    @DisplayName("티켓/초대권이 없는 경우 티켓 금액만큼 현재 갖고 있는 금액에서 빼고 티켓을 갖고 있어야 한다.")
+    fun noHaveTicketAudience() {
+        // given
+        val bag = Bag(audienceInitAmount)
+        val audience = Audience(bag)
+        // when
+        theater.enter(audience)
+        // then
+        assertEquals(audience.bag.amount, audienceInitAmount - ticket.fee)
+        assertTrue(audience.bag.hasTicket)
+    }
+
+    @Test
+    @DisplayName("초대권을 갖고 있는 경우 금액은 그대로고 ticket이 생성돼야 한다.")
+    fun haveTicketAudience() {
+        // given
+        val invitation = Invitation(LocalDateTime.now())
+        val bag = Bag(audienceInitAmount, invitation)
+        val audience = Audience(bag)
+        // when
+        theater.enter(audience)
+        // then
+        assertEquals(audience.bag.amount, audienceInitAmount)
+        assertTrue(audience.bag.hasTicket)
+    }
+
+    companion object {
+        private val ticket = Ticket(10_000L)
+        val tickets = generateSequence(ticket) { it }
+            .take(10)
+            .toMutableList()
+        private const val audienceInitAmount = 50_000L
+    }
+}
