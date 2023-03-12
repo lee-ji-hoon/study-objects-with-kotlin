af05066bf64b535330ce8f39475fbeca2a0caa61
ezhoon <dlwlgns1240@gmail.com>
2023-03-12 15:01:47 +0900

    [1장_이지훈] 소극장 1차 코드 리팩토링 doc 작성

--- src/main/kotlin/chapter01/Theater.kt
+++ src/main/kotlin/chapter01/Theater.kt
@@ -4,6 +4,15 @@ class Theater(
     private val ticketSeller: TicketSeller
 ) {
 
+    /**
+     * TODO 리팩토링 대상이다. 역할과 의존성이 현재 이상하다. -> 소극장(Theater)와 너무 많은 것들이 결합돼있다.
+     * - 소극장(Theater)가 관람객(Audience)의 가방(Audience.Bag)을 직접 확인
+     * - 만약 티켓(Audience.Bag.Ticket)이 없다면 직접 현금(Audience.Bag.Amount)을 꺼내서 지불한다.
+     * - 소극장(Theater)가 판매원(TicketSeller)에게 받은 요금(Ticket.Fee)만큼 Amount를 증가시킨다.
+     * 관람객과 판매원이 소극장의 통제를 너무 과하게 받는 모습으로 보인다.
+     * 현실에서는 관람객이 직접 자신의 가방에서 초대장을 꺼내 판매원에게 건내고, 판매원은 매표소에 있는 티켓을 직접 꺼내서 관람객에게 건내고 돈을 받아 매표소에 보관한다.
+     * @param audience 관람객
+     */
     fun enter(audience: Audience) {
         val ticket = ticketSeller.ticketOffice.getTicket()
 
