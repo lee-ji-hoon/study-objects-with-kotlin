c60c0088f75eb308c541b8a7c2e0eb37773db4e7
ezhoon <dlwlgns1240@gmail.com>
2023-03-26 15:40:06 +0900

    [2장_이지훈] Movie 클래스에서 DiscountPolicy가 변경 가능

--- src/main/kotlin/ezhoon/chapter02/Movie.kt
+++ src/main/kotlin/ezhoon/chapter02/Movie.kt
@@ -1,19 +1,22 @@
 package ezhoon.chapter02
 
 import ezhoon.chapter02.policy.DiscountPolicy
+import ezhoon.chapter02.policy.NoneDiscountPolicy
 import java.time.Duration
 
 data class Movie(
     private val fee: Money,
     private val runningTime: Duration,
     private val title: String,
-    private val discountPolicy: DiscountPolicy
+    private var discountPolicy: DiscountPolicy
 ) {
     fun calculateMovieFee(screening: Screening): Money {
-        if (discountPolicy == null) return fee
-
         return fee.minus(discountPolicy.calculateDiscountAmount(screening))
     }
 
     fun getFee() = fee
+
+    fun changeDiscountPolicy(discountPolicy: DiscountPolicy) {
+        this.discountPolicy = discountPolicy
+    }
 }
