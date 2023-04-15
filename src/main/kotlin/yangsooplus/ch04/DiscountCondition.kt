691cb70694fd3a9fff5d274d22ccebd440b03489
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2023-04-15 18:44:02 +0900

    [4장_양수진] 데이터 중심 설계 리팩토링

--- src/main/kotlin/yangsooplus/ch04/DiscountCondition.kt
+++ src/main/kotlin/yangsooplus/ch04/DiscountCondition.kt
@@ -1,15 +1,26 @@
 package yangsooplus.ch04
 
+import java.lang.IllegalArgumentException
 import java.time.DayOfWeek
 import java.time.LocalTime
 
 class DiscountCondition(
     val type: DiscountConditionType,
-    val sequence: Int,
-    val dayOfWeek: DayOfWeek,
-    val startTime: LocalTime,
-    val endTime: LocalTime
-)
+    private val sequence: Int,
+    private val dayOfWeek: DayOfWeek,
+    private val startTime: LocalTime,
+    private val endTime: LocalTime
+) {
+    fun isDiscountable(dayOfWeek: DayOfWeek, time: LocalTime): Boolean {
+        if (type != DiscountConditionType.PERIOD) throw IllegalArgumentException()
+        return this.dayOfWeek == dayOfWeek && startTime <= time && endTime >= time
+    }
+
+    fun isDiscountable(sequence: Int): Boolean {
+        if (type != DiscountConditionType.SEQUENCE) throw IllegalArgumentException()
+        return this.sequence == sequence
+    }
+}
 
 enum class DiscountConditionType{
     SEQUENCE, PERIOD
--- src/main/kotlin/yangsooplus/ch04/Movie.kt
+++ src/main/kotlin/yangsooplus/ch04/Movie.kt
@@ -2,17 +2,46 @@ package yangsooplus.ch04
 
 import yangsooplus.ch02.Money
 import java.time.Duration
+import java.time.LocalDateTime
 
 class Movie(
     val title: String,
     val runningTime: Duration,
-    val fee: Money,
+    private val fee: Money,
     val discountConditions: List<DiscountCondition>,
 
     val movieType: MovieType,
-    val discountAmount: Money,
-    val discountPercent: Double
-)
+    private val discountAmount: Money,
+    private val discountPercent: Double
+) {
+    fun calculateAmountDiscountedFee(): Money {
+        return fee.minus(discountAmount)
+    }
+
+    fun calculatePercentDiscountedFee(): Money {
+        return fee.minus(fee.times(discountPercent))
+    }
+
+    fun calculateNoneDiscountedFee(): Money = fee
+
+    fun isDiscountable(whenScreened: LocalDateTime, sequence: Int): Boolean {
+        discountConditions.forEach { condition ->
+            when (condition.type) {
+                DiscountConditionType.PERIOD -> {
+                    if (condition.isDiscountable(whenScreened.dayOfWeek, whenScreened.toLocalTime()))
+                        return true
+                }
+
+                DiscountConditionType.SEQUENCE -> {
+                    if (condition.isDiscountable(sequence))
+                        return true
+                }
+            }
+        }
+        return false
+    }
+
+}
 
 enum class MovieType {
     AMOUNT_DISCOUNT, PERCENT_DISCOUNT, NONE_DISCOUNT
