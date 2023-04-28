6e81686fecdb72ca065b73969f1631614fa14612
yangsooplus <yangsooplus@naver.com>
2023-04-28 18:51:51 +0900

    [5장_양수진] 1차 코드 작성

---
+++ src/main/kotlin/yangsooplus/ch05/Movie.kt
@@ -0,0 +1,37 @@
+package yangsooplus.ch05
+
+import yangsooplus.ch02.Money
+import yangsooplus.ch04.MovieType
+import java.time.Duration
+
+class Movie(
+    private val title: String,
+    private val runningTime: Duration,
+    private val fee: Money,
+    private val discountConditions: List<DiscountCondition>,
+    private val movieType: MovieType,
+    private val discountAmount: Money,
+    private val discountPercent: Double
+
+) {
+
+    fun calculateMovieFee(screening: Screening): Money {
+        if (isDiscountable(screening)) {
+            return fee.minus(calculateDiscountAmount())
+        }
+
+        return fee
+    }
+
+    private fun isDiscountable(screening: Screening): Boolean {
+        return discountConditions.any { it.isSatisfiedBy(screening) }
+    }
+
+    private fun calculateDiscountAmount(): Money {
+        return when (movieType) {
+            MovieType.AMOUNT_DISCOUNT -> discountAmount
+            MovieType.PERCENT_DISCOUNT -> fee.times(discountPercent)
+            MovieType.NONE_DISCOUNT -> Money.ZERO
+        }
+    }
+}
