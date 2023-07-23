ad7f5704e1eb534b1963af0360f0a4e1684b635b
ezhoon <dlwlgns1240@gmail.com>
2023-07-23 13:41:39 +0900

    [12장_이지훈] fix Lecture

--- src/main/kotlin/ezhoon/chapter12/Lecture.kt
+++ src/main/kotlin/ezhoon/chapter12/Lecture.kt
@@ -5,7 +5,7 @@ import java.util.*
 open class Lecture(
     private val title: String,
     private val pass: Int,
-    private val scores: List<Int> = ArrayList()
+    open val scores: List<Int> = ArrayList()
 ) {
 
     /** 전체 학생들의 평균 성적을 리턴한다.  */
@@ -14,11 +14,6 @@ open class Lecture(
             .mapToInt { obj: Int -> obj }
             .average()
             .orElse(0.0)
-    }
-
-    /** 전체 학생들의 성적을 리턴한다.  */
-    fun getScores(): List<Int> {
-        return Collections.unmodifiableList(scores)
     }
 
     /** 강의를 이수한 학생의 수와 낙제한 학생의 수를 형식에 맞게 구성한 문자열을 리턴한다.  */
@@ -40,6 +35,6 @@ open class Lecture(
         return String.format("Title: %s, Evaluation Method: %s", title, evaluationMethod)
     }
 
-    open val evaluationMethod: String?
+    open val evaluationMethod: String
         get() = "Pass or Fail"
 }
